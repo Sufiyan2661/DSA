c878f49fe5ee7b5c44665203d44586c5dd3a8f83
Sufiyan2661 <mohdsufiyan99770@gmail.com>
2024-07-01 11:11:22 +0530

    combine array with  the spread operator

---
+++ Basic_Data_Structure/9_nine.js
@@ -0,0 +1,5 @@
+// Combine array with the spread operator
+
+let thisArray = ['sage','rosemary','parsely','thyme']
+let thatArray = ['basil','cilantro',...thisArray,'coriander']
+console.log(thatArray)
